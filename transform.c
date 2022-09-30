b06d005987c9887af833c58c490d56540fe7cfc3
oldosfan <oldosfan@cd5168c8-2ee7-424d-86cc-ab0ed25919d7>
2022-09-30 01:00:09 +0000

    Add files for viewporter support
    * transform.c:
    * viewporter.xml:
    * wp_viewporter.c: New files.
    
    
    git-svn-id: https://svn.code.sf.net/p/twelveto11/code@20 cd5168c8-2ee7-424d-86cc-ab0ed25919d7

---
+++ transform.c
@@ -0,0 +1,177 @@
+/* Wayland compositor running on top of an X server.
+
+Copyright (C) 2022 to various contributors.
+
+This file is part of 12to11.
+
+12to11 is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+12to11 is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with 12to11.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Generic 3x3 matrix transform code.  */
+
+#include <string.h>
+#include <stdio.h>
+
+#include "compositor.h"
+
+#include <X11/extensions/Xrender.h>
+
+/* These macros make column major order easier to reason about for C
+   folks.  */
+#define Index(matrix, row, column)				\
+  ((matrix)[(column) * 3 + (row)])
+
+#define MultiplySub(a, b, a_row, a_column, b_row, b_column)	\
+  (Index (a, a_row, a_column) * Index (b, b_row, b_column))
+
+#if 0
+
+static void
+MatrixPrint (Matrix *matrix)
+{
+  fprintf (stderr,
+	   "%4f %4f %4f\n"
+	   "%4f %4f %4f\n"
+	   "%4f %4f %4f\n\n",
+	   (double) Index (*matrix, 0, 0),
+	   (double) Index (*matrix, 0, 1),
+	   (double) Index (*matrix, 0, 2),
+	   (double) Index (*matrix, 1, 0),
+	   (double) Index (*matrix, 1, 1),
+	   (double) Index (*matrix, 1, 2),
+	   (double) Index (*matrix, 2, 0),
+	   (double) Index (*matrix, 2, 1),
+	   (double) Index (*matrix, 2, 2));
+}
+
+#endif
+
+void
+MatrixMultiply (Matrix a, Matrix b, Matrix *product)
+{
+  Index (*product, 0, 0) = (MultiplySub   (a, b, 0, 0, 0, 0)
+			    + MultiplySub (a, b, 0, 1, 1, 0)
+			    + MultiplySub (a, b, 0, 2, 2, 0));
+  Index (*product, 0, 1) = (MultiplySub   (a, b, 0, 0, 0, 1)
+			    + MultiplySub (a, b, 0, 1, 1, 1)
+			    + MultiplySub (a, b, 0, 2, 2, 1));
+  Index (*product, 0, 2) = (MultiplySub   (a, b, 0, 0, 0, 2)
+			    + MultiplySub (a, b, 0, 1, 1, 2)
+			    + MultiplySub (a, b, 0, 2, 2, 2));
+
+  Index (*product, 1, 0) = (MultiplySub   (a, b, 1, 0, 0, 0)
+			    + MultiplySub (a, b, 1, 1, 1, 0)
+			    + MultiplySub (a, b, 1, 2, 2, 0));
+  Index (*product, 1, 1) = (MultiplySub   (a, b, 1, 0, 0, 1)
+			    + MultiplySub (a, b, 1, 1, 1, 1)
+			    + MultiplySub (a, b, 1, 2, 2, 1));
+  Index (*product, 1, 2) = (MultiplySub   (a, b, 1, 0, 0, 2)
+			    + MultiplySub (a, b, 1, 1, 1, 2)
+			    + MultiplySub (a, b, 1, 2, 2, 2));
+
+  Index (*product, 2, 0) = (MultiplySub   (a, b, 2, 0, 0, 0)
+			    + MultiplySub (a, b, 2, 1, 1, 0)
+			    + MultiplySub (a, b, 2, 2, 2, 0));
+  Index (*product, 2, 1) = (MultiplySub   (a, b, 2, 0, 0, 1)
+			    + MultiplySub (a, b, 2, 1, 1, 1)
+			    + MultiplySub (a, b, 2, 2, 2, 1));
+  Index (*product, 2, 2) = (MultiplySub   (a, b, 2, 0, 0, 2)
+			    + MultiplySub (a, b, 2, 1, 1, 2)
+			    + MultiplySub (a, b, 2, 2, 2, 2));
+}
+
+void
+MatrixIdentity (Matrix *matrix)
+{
+  memset (matrix, 0, sizeof *matrix);
+
+  Index (*matrix, 0, 0) = 1.0f;
+  Index (*matrix, 1, 1) = 1.0f;
+  Index (*matrix, 2, 2) = 1.0f;
+}
+
+void
+MatrixTranslate (Matrix *transform, float tx, float ty)
+{
+  Matrix temp, copy;
+
+  MatrixIdentity (&temp);
+  memcpy (copy, transform, sizeof copy);
+
+  /* Set the tx and ty.  */
+  Index (temp, 0, 2) = tx;
+  Index (temp, 1, 2) = ty;
+
+  /* Multiply it with the transform.  */
+  MatrixMultiply (copy, temp, transform);
+}
+
+void
+MatrixScale (Matrix *transform, float sx, float sy)
+{
+  Matrix temp, copy;
+
+  MatrixIdentity (&temp);
+  memcpy (copy, transform, sizeof copy);
+
+  /* Set the scale factors.  */
+  Index (temp, 0, 0) = sx;
+  Index (temp, 1, 1) = sy;
+
+  /* Multiply it with the transform.  */
+  MatrixMultiply (copy, temp, transform);
+}
+
+void
+MatrixExport (Matrix *transform, XTransform *xtransform)
+{
+  /* M1 M2 M3     X
+     M4 M5 M6   * Y
+     M7 M8 M9     Z
+
+     =
+
+     M1*X + M2*Y + M3*1 = X1
+     M4*X + M5*Y + M6*1 = Y1
+     M7*X + M8*Y + M9*1 = Z1 (Only on some drivers)
+
+     where
+
+     M1 = matrix[0][0]
+     M2 = matrix[0][1]
+     M3 = matrix[0][2]
+     M4 = matrix[1][0]
+     M5 = matrix[1][1]
+     M6 = matrix[1][2]
+     M7 = matrix[2][0]
+     M8 = matrix[2][1]
+     M9 = matrix[2][2]  */
+
+#define Export(row, column)						\
+  xtransform->matrix[row][column]					\
+    = XDoubleToFixed (Index (*transform, row, column))
+
+  Export (0, 0);
+  Export (0, 1);
+  Export (0, 2);
+
+  Export (1, 0);
+  Export (1, 1);
+  Export (1, 2);
+
+  Export (2, 0);
+  Export (2, 1);
+  Export (2, 2);
+
+#undef Export
+}
---
+++ wp_viewporter.c
@@ -0,0 +1,266 @@
+/* Wayland compositor running on top of an X server.
+
+Copyright (C) 2022 to various contributors.
+
+This file is part of 12to11.
+
+12to11 is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+12to11 is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with 12to11.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include <string.h>
+
+#include "compositor.h"
+#include "viewporter.h"
+
+typedef struct _ViewportExt ViewportExt;
+
+struct _ViewportExt
+{
+  /* The attached surface.  */
+  Surface *surface;
+
+  /* The attached callback.  */
+  DestroyCallback *destroy_callback;
+
+  /* The associated wl_resource.  */
+  struct wl_resource *resource;
+};
+
+/* The wp_viewporter global.  */
+static struct wl_global *viewporter_global;
+
+static void
+HandleResourceDestroy (struct wl_resource *resource)
+{
+  ViewportExt *ext;
+
+  ext = wl_resource_get_user_data (resource);
+
+  /* Free the user data and detach the surface destroy callback.  */
+
+  if (ext->surface)
+    {
+      XLSurfaceCancelRunOnFree (ext->destroy_callback);
+      ext->surface->viewport = NULL;
+
+      /* Clear viewport data.  */
+      ext->surface->pending_state.pending |= PendingViewportSrc;
+      ext->surface->pending_state.pending |= PendingViewportDest;
+      ext->surface->pending_state.src_x = -1.0;
+      ext->surface->pending_state.src_y = -1.0;
+      ext->surface->pending_state.src_width = -1.0;
+      ext->surface->pending_state.src_height = -1.0;
+      ext->surface->pending_state.dest_width = -1;
+      ext->surface->pending_state.dest_height = -1;
+    }
+
+  XLFree (ext);
+}
+
+static void
+HandleSurfaceDestroy (void *data)
+{
+  ViewportExt *ext;
+
+  ext = data;
+
+  /* Clear callbacks and surface, as they are now gone.  */
+  ext->surface = NULL;
+  ext->destroy_callback = NULL;
+}
+
+static void
+DestroyViewport (struct wl_client *client, struct wl_resource *resource)
+{
+  wl_resource_destroy (resource);
+}
+
+static void
+SetSource (struct wl_client *client, struct wl_resource *resource,
+	   wl_fixed_t x, wl_fixed_t y, wl_fixed_t width, wl_fixed_t height)
+{
+  ViewportExt *ext;
+  double src_x, src_y, src_width, src_height;
+
+  ext = wl_resource_get_user_data (resource);
+
+  /* Check that the surface is still attached.  */
+  if (!ext->surface)
+    {
+      wl_resource_post_error (resource, WP_VIEWPORT_ERROR_NO_SURFACE,
+			      "the surface has been detached");
+      return;
+    }
+
+  src_x = wl_fixed_to_double (x);
+  src_y = wl_fixed_to_double (y);
+  src_width = wl_fixed_to_double (width);
+  src_height = wl_fixed_to_double (height);
+
+  /* Now, verify that the values are correct.  They can either all be
+     -1, or the origin must be positive, and width and height must be
+     more than 0.  */
+  if (!(src_x == -1.0 && src_y == -1.0 && src_width == -1.0
+	&& src_height == -1.0)
+      && (src_x < 0 || src_y < 0
+	  || src_width < 1.0 || src_height < 1.0))
+    wl_resource_post_error (resource, WP_VIEWPORT_ERROR_BAD_VALUE,
+			    "invalid source rectangle specified");
+
+  ext->surface->pending_state.pending |= PendingViewportSrc;
+  ext->surface->pending_state.src_x = src_x;
+  ext->surface->pending_state.src_y = src_y;
+  ext->surface->pending_state.src_width = src_width;
+  ext->surface->pending_state.src_height = src_height;
+}
+
+static void
+SetDestination (struct wl_client *client, struct wl_resource *resource,
+		int32_t width, int32_t height)
+{
+  ViewportExt *ext;
+
+  ext = wl_resource_get_user_data (resource);
+
+  /* Check that the surface is still attached.  */
+  if (!ext->surface)
+    {
+      wl_resource_post_error (resource, WP_VIEWPORT_ERROR_NO_SURFACE,
+			      "the surface has been detached");
+      return;
+    }
+
+  if ((width <= 0 || height <= 0)
+      && !(width == -1 && height == -1))
+    {
+      wl_resource_post_error (resource, WP_VIEWPORT_ERROR_BAD_VALUE,
+			      "invalid destination size specified");
+      return;
+    }
+
+  ext->surface->pending_state.pending |= PendingViewportDest;
+  ext->surface->pending_state.dest_width = width;
+  ext->surface->pending_state.dest_height = height;
+}
+
+static const struct wp_viewport_interface wp_viewport_impl =
+  {
+    .destroy = DestroyViewport,
+    .set_source = SetSource,
+    .set_destination = SetDestination,
+  };
+
+
+
+static void
+Destroy (struct wl_client *client, struct wl_resource *resource)
+{
+  wl_resource_destroy (resource);
+}
+
+static void
+GetViewport (struct wl_client *client, struct wl_resource *resource,
+	     uint32_t id, struct wl_resource *surface_resource)
+{
+  ViewportExt *ext;
+  Surface *surface;
+
+  surface = wl_resource_get_user_data (surface_resource);
+
+  /* If the surface already has a viewport resource attached, post an
+     error.  */
+  if (surface->viewport)
+    {
+      wl_resource_post_error (resource, WP_VIEWPORTER_ERROR_VIEWPORT_EXISTS,
+			      "viewport already exists");
+      return;
+    }
+
+  ext = XLSafeMalloc (sizeof *ext);
+
+  if (!ext)
+    {
+      wl_resource_post_no_memory (resource);
+      return;
+    }
+
+  memset (ext, 0, sizeof *ext);
+  ext->resource
+    = wl_resource_create (client, &wp_viewport_interface,
+			  wl_resource_get_version (resource), id);
+
+  if (!ext->resource)
+    {
+      XLFree (ext);
+      wl_resource_post_no_memory (resource);
+      return;
+    }
+
+  /* Attach the surface.  */
+  ext->surface = wl_resource_get_user_data (surface_resource);
+  ext->destroy_callback
+    = XLSurfaceRunOnFree (ext->surface, HandleSurfaceDestroy,
+			  ext);
+  surface->viewport = ext;
+
+  wl_resource_set_implementation (ext->resource, &wp_viewport_impl,
+				  ext, HandleResourceDestroy);
+}
+
+static const struct wp_viewporter_interface wp_viewporter_impl =
+  {
+    .destroy = Destroy,
+    .get_viewport = GetViewport,
+  };
+
+
+
+static void
+HandleBind (struct wl_client *client, void *data, uint32_t version,
+	    uint32_t id)
+{
+  struct wl_resource *resource;
+
+  resource = wl_resource_create (client, &wp_viewporter_interface,
+				 version, id);
+  if (!resource)
+    {
+      wl_client_post_no_memory (client);
+      return;
+    }
+
+  wl_resource_set_implementation (resource, &wp_viewporter_impl, NULL,
+				  NULL);
+}
+
+void
+XLInitWpViewporter (void)
+{
+  viewporter_global = wl_global_create (compositor.wl_display,
+					&wp_viewporter_interface,
+					1, NULL, HandleBind);
+}
+
+void
+XLWpViewportReportBadSize (ViewportExt *ext)
+{
+  wl_resource_post_error (ext->resource, WP_VIEWPORT_ERROR_BAD_SIZE,
+			  "invalid non-integer size specified");
+}
+
+void
+XLWpViewportReportOutOfBuffer (ViewportExt *ext)
+{
+  wl_resource_post_error (ext->resource, WP_VIEWPORT_ERROR_OUT_OF_BUFFER,
+			  "viewport source rectangle out of buffer");
+}
